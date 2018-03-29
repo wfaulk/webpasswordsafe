16fb91e616535331f24e3df3ce2d849486b690fe
William Faulk <wfaulk@us.ibm.com>
2018-03-29 11:13:33 -0400

    add dependencies

---
+++ dependencies/local/install-local.sh
@@ -0,0 +1,3 @@
+mvn install:install-file -DgroupId=com.extjs -DartifactId=gxt -Dversion=2.2.5 -Dpackaging=jar -Dfile=gxt-2.2.5-gwt22.jar
+mvn install:install-file -DgroupId=net.sf.gwt-widget -DartifactId=gxt-sl -Dversion=1.1 -Dpackaging=jar -Dfile=gwt-sl-1.1.jar
+mvn install:install-file -DgroupId=trove -DartifactId=trove -Dversion=2.0.4 -Dpackaging=jar -Dfile=trove-2.0.4.jar
